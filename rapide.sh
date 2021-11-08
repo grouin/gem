de17fa15c03f01f0f802e6603cec1523dc7f0a47
Cyril Grouin <cyril.grouin@limsi.fr>
2021-11-08 14:05:27 +0100

    Ajout d'options dans le lanceur rapide.sh
    - all / visu / emo / tag

--- rapide.sh
+++ rapide.sh
@@ -2,31 +2,61 @@
 
 racine=~/Bureau/projet-GEM/corpus/ina/GMMP/radio/lium_asr_xml/
 
-perl conversion-xml-to-tab.pl $racine/ 0.5
+case $1 in
 
-for fichier in `ls $racine/*tab`
-do
-    cat $fichier | sed "s/^$/§/g" >`echo $fichier | sed "s/tab/tok/"`
-done
-for fichier in `ls $racine/*tok`
-do
-    ../../treetagger/bin/tree-tagger -token -lemma -no-unknown ../../treetagger/lib/french.par $fichier >`echo $fichier | sed "s/tok/tag/"`
-done
-for fichier in `ls $racine/*tag`
-do
-    cat $fichier | sed "s/§\t.*\t§//g" >`echo $fichier | sed "s/tag/tok/"`
-done
+    visu) perl visualisation-code-barre_v5.pl $racine visu-emo.html $2;;
 
+    conv) perl conversion-xml-to-tab.pl $racine/ 0.5;;
 
-perl fouille-emotions-colonne.pl $racine
+    tag)
+	for fichier in `ls $racine/*tab`
+	do
+	    cat $fichier | sed "s/^$/§/g" >`echo $fichier | sed "s/tab/tok/"`
+	done
+	for fichier in `ls $racine/*tok`
+	do
+	    ../../treetagger/bin/tree-tagger -token -lemma -no-unknown ../../treetagger/lib/french.par $fichier >`echo $fichier | sed "s/tok/tag/"`
+	done
+	for fichier in `ls $racine/*tag`
+	do
+	    cat $fichier | sed "s/§\t.*\t§//g" >`echo $fichier | sed "s/tag/tok/"`
+	done
+	;;
 
-for fichier in `ls $racine/*gen`
-do
-    paste $fichier `echo $fichier | sed "s/gen/time/"` `echo $fichier | sed "s/gen/emo/"` >`echo $fichier | sed "s/gen/paste/"`
-done
+    emo) perl fouille-emotions-colonne.pl $racine
+	 for fichier in `ls $racine/*gen`
+	 do
+	     paste $fichier `echo $fichier | sed "s/gen/time/"` `echo $fichier | sed "s/gen/emo/"` >`echo $fichier | sed "s/gen/paste/"`
+	 done
+	 ;;
+    
+    all)
+	perl conversion-xml-to-tab.pl $racine/ 0.5
 
-perl modifiePasteTSV.pl $racine
+	for fichier in `ls $racine/*tab`
+	do
+	    cat $fichier | sed "s/^$/§/g" >`echo $fichier | sed "s/tab/tok/"`
+	done
+	for fichier in `ls $racine/*tok`
+	do
+	    ../../treetagger/bin/tree-tagger -token -lemma -no-unknown ../../treetagger/lib/french.par $fichier >`echo $fichier | sed "s/tok/tag/"`
+	done
+	for fichier in `ls $racine/*tag`
+	do
+	    cat $fichier | sed "s/§\t.*\t§//g" >`echo $fichier | sed "s/tag/tok/"`
+	done
 
-perl visualisation-code-barre_v5.pl $racine visu-emo.html
+	perl fouille-emotions-colonne.pl $racine
 
-echo "Terminé"
+	for fichier in `ls $racine/*gen`
+	do
+	    paste $fichier `echo $fichier | sed "s/gen/time/"` `echo $fichier | sed "s/gen/emo/"` >`echo $fichier | sed "s/gen/paste/"`
+	done
+
+	perl modifiePasteTSV.pl $racine
+
+	perl visualisation-code-barre_v5.pl $racine visu-emo.html
+
+	echo "Terminé"
+	;;
+esac
